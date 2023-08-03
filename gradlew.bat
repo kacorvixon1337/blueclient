6554454f313b33d5ddde9d0d80c2761df04a89b5
Tryfle <111710533+Tryflle@users.noreply.github.com>
2023-08-02 20:23:19 -0400

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -52,8 +52,8 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JDK_17:"=%
-set JAVA_EXE=%JDK_17%/bin/java.exe
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
